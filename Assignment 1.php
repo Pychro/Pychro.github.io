3390d97c46052d349bb8911f05a699fa00c59922
Pychro <herrin04@nsuok.edu>
2020-01-17 15:47:59 -0600

    Update Assignment 1.php

--- Assignment 1.php
+++ Assignment 1.php
@@ -11,3 +11,12 @@ $result = 15;
 Trevor Herring <p></p>
 
 
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+    Number 1: <input type="text" name="number1" value="15"> <p></p>
+    Number 2: <input type="text" name="number2" value="15"> <p></p>
+
+    <input type="submit" value="Calculate Answer"> <p></p>
+
+
+</form>
